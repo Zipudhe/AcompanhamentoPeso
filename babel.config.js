97a6ea7d09daebc2c1dc8a347c92d98fcae8bb9a
zipudhe <lucasramalho29@gmail.com>
2023-01-25 23:21:48 -0300

    ✨ basic config

--- babel.config.js
+++ babel.config.js
@@ -2,5 +2,26 @@ module.exports = function(api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extension: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+          alias: { "@components": "./src/components" }
+        }
+      ],
+      [
+        'module:react-native-dotenv',
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          safe: false,
+          allowUndefined: true,
+          verbose: false
+        }
+      ]
+    ]
   };
 };
